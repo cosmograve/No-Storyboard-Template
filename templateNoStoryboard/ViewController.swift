3cc475d51f7c6665e28f5ff560c444931b487a18
Алексей Авер <aleksey.aver@testdlab.com>
2022-07-03 14:03:10 +0700

    create

---
+++ templateNoStoryboard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  templateNoStoryboard
+//
+//  Created by Алексей Авер on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+
+    }
+
+
+}
+
